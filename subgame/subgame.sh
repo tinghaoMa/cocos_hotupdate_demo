e643dfed5f54c08ef229cbc937d89ebc65ac08a6
tinghaoma <tinghaoma@sohu-inc.com>
2018-01-05 21:56:23 +0800

    修复异常

--- subgame/subgame.sh
+++ subgame/subgame.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
+rm -rf ../hotupdate/public/remote-assets/subgame/peision.manifest
+rm -rf ../hotupdate/public/remote-assets/subgame/version.manifest
+rm -rf ../hotupdate/public/remote-assets/subgame/res/*
+rm -rf ../hotupdate/public/remote-assets/subgame/src/*
+
 CocosCreator --path .  --build "2d2f792f-a40c-49bb-a189-ed176a246e49;debug=false;title=hello_world;packageName=org.cocos2d.helloworld;inlineSpriteFrames=true"
 
 rm build/jsb-default/src/jsb_polyfill.*
 
 ./1.sh
 
-rm -rf ../hotupdate/public/remote-assets/subgame/res
-rm -rf ../hotupdate/public/remote-assets/subgame/src
+
 
 cp -r ./build/jsb-default/res/  ../hotupdate/public/remote-assets/subgame/res
 cp -r ./build/jsb-default/src/  ../hotupdate/public/remote-assets/subgame/src
